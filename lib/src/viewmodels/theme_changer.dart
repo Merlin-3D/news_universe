65b6334ff9be57960e5e5d02e294b9c172a60998
Merlin-3D <axel.deffo@gmail.com>
2023-06-07 13:13:28 +0100

    fix: Remove import.

--- lib/src/viewmodels/theme_changer.dart
+++ lib/src/viewmodels/theme_changer.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_universe/src/core/theming/dimens.dart';
 import 'package:news_universe/src/core/theming/theme.dart';
-import 'package:news_universe/src/core/theming/theme_colors.dart';
-import 'package:news_universe/src/shared/utils/common.dart';
-import 'package:news_universe/src/shared/utils/sized_extension.dart';
 
 class ThemeChanger with ChangeNotifier {
   ThemeData _themeData;
